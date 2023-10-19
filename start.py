0cab4a51225e999fdf35671526364f1298543e0d
junran <kingsleyl0107@gmail.com>
2023-10-19 13:46:07 +0800

    add progress bar

--- start.py
+++ start.py
@@ -2,7 +2,9 @@ from config.config import FEATURE_YEAR
 import pandas as pd
 import geopandas as gpd
 from spider.crawler import GSV_Crawler
+from tqdm import tqdm
 
+tqdm.pandas(desc='pandas bar')
 if __name__ == "__main__":
     Crawler = GSV_Crawler()
     accident = pd.read_csv(r'./2022_accident.csv')[['Easting',
@@ -10,6 +12,6 @@ if __name__ == "__main__":
     accident['geometry'] = gpd.points_from_xy(
         accident["Easting"], accident["Northing"],
         crs="epsg:27700").to_crs("epsg:4326")
-    accident['GSV_ID'] = accident['geometry'].apply(
+    accident['GSV_ID'] = accident['geometry'].progress_apply(
         lambda x: Crawler.get_SVI(Crawler.get_panoid(x)))
     accident.to_csv(rf"./{FEATURE_YEAR}_GSV/recorder.csv")
